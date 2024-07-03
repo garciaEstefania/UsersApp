94d6954c44852b34e0c133f3333a2ea1397b4ffc
Estefania Garcia <garcia_estefannia@hotmail.com>
2024-07-03 13:48:42 -0600

    Add project setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module:react-native-dotenv'],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          '~components': './src/components',
+          '~navigation': './src/navigation',
+          '~redux': './src/redux',
+          '~screens': './src/screens',
+          '~helpers': './src/helpers',
+          '~hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
